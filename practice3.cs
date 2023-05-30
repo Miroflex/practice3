190b7d7ce8a60ee84e7c3b4a76671529904b437f
Miroflex <108157301+Miroflex@users.noreply.github.com>
2023-05-30 16:20:07 +0300

    Add files via upload

---
+++ practice3.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+
+namespace Shark
+
+{
+
+    internal class Program
+
+    {
+
+        public static void Main(string[] args)
+
+        {
+
+            Console.WriteLine("Введіть речення: ");
+
+            string text = Console.ReadLine();
+
+            char[] charArray = text.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            string reversetext = new string(charArray);
+
+            Console.WriteLine("Речення у зворотному порядку: ");
+
+            Console.WriteLine(reversetext);
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
